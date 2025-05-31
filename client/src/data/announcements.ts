7f5b08e1aa2eebb319a083dddd7617701888c841
myhealthbook-co <myhealthrestart@gmail.com>
2025-05-31 19:56:39 +0530

    Update announcements.ts

--- client/src/data/announcements.ts
+++ client/src/data/announcements.ts
@@ -11,8 +11,8 @@ export const announcements: Announcement[] = [
   {
     id: 1,
     title: "Revised Business Hours",
-    content: "Please be informed that effective 2nd January 2025, our company's working days and hours (except Public Holiday) will be as follows:\n\nWorking Days: Monday to Thursday\nWorking Hours: 9:30 AM to 7:00 PM\n\nWe are confident that this revised working days and hours will be able to meet your needs and provide you the necessary service.",
-    date: "2025-01-02",
+    content: "Please be informed that effective 23nd January 2025, our company's working days and hours (except Public Holiday) will be as follows:\n\nWorking Days: Monday(10am to 2pm & 5pm to 9pm) to Sunday(2nd half holiday)\n\nWe are confident that this revised working days and hours will be able to meet your needs and provide you the necessary service.",
+    date: "2025-03-23",
     isImportant: true
   }
 ];
